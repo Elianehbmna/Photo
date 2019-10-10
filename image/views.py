7f74f7b6e8f79c24efabc89d081f36dec279d479
Elianehbmna <elianehabimana3@gmail.com>
2019-10-10 10:05:50 +0200

    Template Inheritance

--- image/views.py
+++ image/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
 
@@ -10,17 +10,7 @@ def welcome(request):
 
 def image_day(request):
     date = dt.date.today()
-
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Images for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request, 'all-image/today-image.html', {"date": date,})
 
 def convert_dates(dates):
 
@@ -34,21 +24,16 @@ def convert_dates(dates):
     return day
 
 def past_days_image(request,past_date):
-    # Converts data from the string Url
     try:
         # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
 
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
+        assert False
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Image for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-image/past-image.html', {"date": date})
