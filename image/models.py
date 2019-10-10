4f8886f15054b1c125ce87b46c087d1c753d5e58
Elianehbmna <elianehabimana3@gmail.com>
2019-10-10 14:14:38 +0200

    creating models

--- image/models.py
+++ image/models.py
@@ -2,5 +2,68 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import datetime as dt
+
+class Location(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def update_location(self):
+        self.delete()
+
+class Category(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def update_category(self):
+        self.delete()
+
+
+
+
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    location = models.ForeignKey(Location, blank=True)
+    category = models.ForeignKey(Category, blank=True)
+    
+    def __str__(self):
+       return self.name
+
+    def save_image(self):
+        self.save()
+    
+    @classmethod
+    def delete_image(cls,id):
+        images = cls.objects.filter(id=id)
+        images.delete()
+    
+    def get_image_by_id(id):
+        images = cls.objects.filter(id=id)
+        return images
+
+    def filter_by_location(location):
+        images = cls.objects.filter(location=location)
+        return images
+
+
+
