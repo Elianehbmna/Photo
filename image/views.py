7539a923679618d92ea8a1a6bda850a382b601a7
Elianehbmna <elianehabimana3@gmail.com>
2019-10-10 09:51:22 +0200

    creating templates folder

--- image/views.py
+++ image/views.py
@@ -6,7 +6,7 @@ import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request, 'welcome.html')
 
 def image_day(request):
     date = dt.date.today()
