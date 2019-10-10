0038f98089a21afdb415e03565763f1002af28d1
Elianehbmna <elianehabimana3@gmail.com>
2019-10-10 15:22:55 +0200

    testing for the first time

--- image/tests.py
+++ image/tests.py
@@ -2,5 +2,26 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from .models import Location,Category,Image
 
-# Create your tests here.
+class LocationTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.remera= Location(name="Remera")
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.remera,Location))
+    
+    # Testing Save Method
+    def test_save_location(self):
+        self.remera.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+    
+    def test_delete_location(self):
+        self.remera.delete_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
