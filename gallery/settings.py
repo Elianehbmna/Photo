b9f583f42e28357dc842d7853cbe195bd291e0d8
Elianehbmna <elianehabimana3@gmail.com>
2019-10-10 10:13:34 +0200

    adding bootstrap

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'image',
+    'bootstrap3',
     'image.apps.ImageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
