43ab9253bbfaa1c17494c94fcf64537d4b4cfa63
Elianehbmna <elianehabimana3@gmail.com>
2019-10-13 23:39:45 +0200

    deployment on heroku

--- image/urls.py
+++ image/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 urlpatterns=[
     
     url(r'^$',views.image_display,name='images'),
-    url(r'^search/', views.search_results, name='search_results'),
-    url(r'^image/(\d+)',views.image,name ='image')
+    url(r'^search/', views.search_results, name='search_results')
+   
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
